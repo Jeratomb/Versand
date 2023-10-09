32b75ec3b43cb04067d50870d5616d4d9d7c8df0
Jeratomb <rayen.phillip@googlemail.com>
2023-10-09 22:01:26 +0200

    loadSlider(), loadRdb(), optical changes

--- src/main/java/com/example/versand/HelloController.java
+++ src/main/java/com/example/versand/HelloController.java
@@ -1,14 +1,136 @@
 package com.example.versand;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.scene.Group;
+import javafx.scene.control.*;
+import javafx.scene.shape.Line;
+
+import java.time.LocalDate;
 
 public class HelloController {
-    @FXML
-    private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private TitledPane tpFrom;
+    @FXML
+    private TextField txtFname;
+    @FXML
+    private TextField txtFstr;
+    @FXML
+    private TextField txtFloc;
+    @FXML
+    private TextField txtFplz;
+    @FXML
+    private TextField txtFLname;
+    @FXML
+    private TextField txtFstrNr;
+    @FXML
+    private TitledPane tpIns;
+    @FXML
+    private CheckBox chckIns;
+    @FXML
+    private RadioButton rdb100;
+    @FXML
+    private RadioButton rdb500;
+    @FXML
+    private RadioButton rdbO500;
+    @FXML
+    private TextField txtAmount;
+    @FXML
+    private TitledPane tpCalc;
+    @FXML
+    private Button btnCalc;
+    @FXML
+    private TitledPane tpDel;
+    @FXML
+    private CheckBox chckExp;
+    @FXML
+    private RadioButton rdbLett;
+    @FXML
+    private RadioButton rdbPac;
+    @FXML
+    private RadioButton rdbBPac;
+    @FXML
+    private CheckBox chckAlt;
+    @FXML
+    private DatePicker dtpDel;
+    @FXML
+    private TitledPane tpTo;
+    @FXML
+    private TextField txtTname;
+    @FXML
+    private TextField txtTstr;
+    @FXML
+    private TextField txtTloc;
+    @FXML
+    private TextField txtTplz;
+    @FXML
+    private TextField txtTLname;
+    @FXML
+    private TextField txtTstrNr;
+    @FXML
+    private TextField txtDesc;
+    @FXML
+    private TextField txtID;
+    @FXML
+    private DatePicker dtpCom;
+    @FXML
+    private Button btnSave;
+    @FXML
+    private Button btnLoad;
+    @FXML
+    private Label lblStatus;
+    @FXML
+    private Slider sldDisc;
+
+
+    @FXML
+    public void initialize() {
+        loadSlider();
+        loadRdb();
+        dtpCom.setValue(LocalDate.now());
+    }
+    @FXML
+    public void onbtnCalc(ActionEvent actionEvent) {
+    }
+
+    @FXML
+    public void onbtnSave(ActionEvent actionEvent) {
+    }
+    @FXML
+    public void onbtnLoad(ActionEvent actionEvent) {
+    }
+
+    @FXML
+    public void loadSlider() {
+        sldDisc.setMax(10);
+        sldDisc.setMin(0);
+        sldDisc.setShowTickMarks(true);
+        sldDisc.setShowTickLabels(true);
+        sldDisc.setMajorTickUnit(1);
+        sldDisc.setMinorTickCount(0);
+        sldDisc.setBlockIncrement(1);
+    }
+
+    @FXML
+    public void loadRdb(){
+        ToggleGroup insToggleGroup = new ToggleGroup();
+        ToggleGroup delToggleGroup = new ToggleGroup();
+
+        rdb100.setToggleGroup(insToggleGroup);
+        rdb500.setToggleGroup(insToggleGroup);
+        rdbO500.setToggleGroup(insToggleGroup);
+        
+        rdb100.setUserData("100");
+        rdb500.setUserData("500");
+        rdbO500.setUserData("501");
+
+        rdbLett.setToggleGroup(delToggleGroup);
+        rdbPac.setToggleGroup(delToggleGroup);
+        rdbBPac.setToggleGroup(delToggleGroup);
+
+        rdbLett.setUserData("Lett");
+        rdbPac.setUserData("Pac");
+        rdbBPac.setUserData("BPac");
     }
 }
